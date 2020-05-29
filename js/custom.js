0c131c866dd3ccf738f819eaa050b12270556aff
Simon Barthmuß <quaSimba@posteo.net>
2020-05-29 18:38:40 +0200

    * Implement ajax base * Include rudimentary burger image and 'welcome text' with ajax

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,12 @@
 // kick-starting fullpage
-$(document).ready(function() {
+$(document).ready(
+  function() {
+    initFullpage();
+    loadWelcome();
+  }
+);
+
+function initFullpage() {
   $('#fullpage').fullpage({
     //options here
     autoScrolling: false,
@@ -9,4 +16,16 @@ $(document).ready(function() {
 
   //methods
   $.fn.fullpage.setAllowScrolling(false);
-});
+}
+
+function loadWelcome() {
+  $('#upper-text').load('ajax/texts.html #welcome-text', ajaxError());
+  $('#burger-container').load('ajax/burger-background.html #burger', ajaxError());
+}
+
+function ajaxError(response, status, xhr) {
+  if (status == "error") {
+    var msg = "Sorry but there was an error: ";
+    console.log(msg + xhr.status + " " + xhr.statusText);
+  }
+}
