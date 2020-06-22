296580981e155a23d883cebe2d404b474558b956
Shirimahler <shirimahler@gmail.com>
2020-06-22 15:34:39 +0200

    Text in html
    Different htmi
    Info
    contract
    nice try
    graph
    
    Var - tpb - hours for BM

--- js/flag-events.js
+++ js/flag-events.js
@@ -3,40 +3,46 @@ $("input.flag").on('click', function(event) {
   /* Act on the event */
   switch (event.target.id) {
     case 'flag-US':
-          country = 'America';
-          greetingFormular = 'Hey';
-          salary = '9.03';
-          employeeNumber = '#210,001';
+      country = 'America';
+      greetingFormular = 'Hey';
+      salary = '9.03';
+      employeeNumber = '#210,001';
+      tpb = '00:36:61';
       break;
     case 'flag-ME':
       country = 'Mexico';
       greetingFormular = 'Hola';
       salary = '1';
       employeeNumber = '#11,501';
+      tpb = '02:28:00';
       break;
     case 'flag-IT':
       country = 'Italy';
       greetingFormular = 'Ciao';
       salary = '9.2';
       employeeNumber = '#3,045';
+      tpb = '00:30:65';
       break;
     case 'flag-ZA':
       country = 'South Africa';
       greetingFormular = 'Hallo';
       salary = '0.97';
       employeeNumber = '#10,001';
+      tpb = '03:17:94';
       break;
     case 'flag-IN':
       country = 'India';
       greetingFormular = 'Namaste';
       salary = '0.66';
       employeeNumber = '#4,001';
+      tpb = '03:35:45';
       break;
     case 'flag-AU':
       country = 'Australia';
       greetingFormular = 'G׳day';
       salary = '13.4';
       employeeNumber = '#90,001';
+      tpb = '00:17:46';
       break;
     default:
       country = 'Mid World';
